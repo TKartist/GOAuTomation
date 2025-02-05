5417fb19a01fbfde49035b5b56d1d1cca4312caa
TKartist <hunr2001@gmail.com>
2025-02-05 17:18:18 +0100

    got the first draft running

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from text_extractor import organize_bucket, pdf_to_text
 import ast
 from openai_caller import conversation
+import asyncio
 
 def check_update():
     pdfs = os.listdir("document_folder")
@@ -17,27 +18,26 @@ def summarize(filename):
             text = f.read()
             f.close()
         pages = ast.literal_eval(text)
-        conversation(pages)
-
     except Exception as e:
         print(f"Found an error while loading {filename} to pass to OpenAI API:\n{e}")
         return
+    doc_summary = asyncio.run(conversation(pages))
+    return doc_summary
+    
 
 def main():
     '''
     Read documents from 'document_folder' and convert the pdf files to html
     proceed to organize them and store them in output folder
     '''
-    if check_update():
-        pdfs = os.listdir("document_folder")
-        for pdf in pdfs:
-            bucket = pdf_to_text(f"document_folder/{pdf}")
-            clean_bucket = organize_bucket(bucket)
-            with open(f"output/{pdf.split(".")[0]}.txt", "w") as f:
-                f.write(str(clean_bucket))
-                f.close()
-        print("All PDF files are converted to coordinated texts and are stored in output")
-    summarize("secret_info.txt")
+    files = os.listdir("output")
+    outputs = []
+    for file in files:
+        outputs.append(summarize(f"output/{file}"))
+    with open("summary_file.txt", "w") as f:
+        f.write(str(outputs))
+        f.close()
+    print("ALL FILES SUMMARIZED")
 
 if __name__ == "__main__":
     main()
--- openai_caller.py
+++ openai_caller.py
@@ -14,44 +14,36 @@ def collect_system_message():
     return system_message
         
 
-
-def asking_openai(prompt, secrets):
-    openai.api_type = "azure"
-
-    # Azure OpenAI endpoint
-    openai.api_base = secrets["Target URI"]
-
-    # Use environment variables for security
-    openai.api_key = os.environ.get("OPENAI_API_KEY")  
-    openai.api_version = secrets["Model version"] 
-    # Deployment name
+async def conversation(pages):
+    system_message = " ".join(collect_system_message().split("\n"))
+    secrets = secret_extraction()
+    prompt = [{
+        "role" : "system",
+        "content" : system_message
+    }]
     deployment_name = secrets["Deployment name"]
-    """Send a prompt to Azure OpenAI ChatGPT API"""
-    response = openai.ChatCompletion.create(
-        engine=deployment_name,  
-        messages=[
-            {"role": "system", "content": "you are a helpful assistant"},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=4096,
-        # temperature decides whether if the response is more literal or creative
-        # lower is more literal, and higher is more creative answer
-        # some contexts may require derivation so I chose the middle value
-        temperature=0.5 
+    client = openai.AsyncAzureOpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY"),
+        azure_endpoint="https://ifrcorg-go.openai.azure.com/",
+        api_version="2024-08-01-preview"
     )
-    return response["choices"][0]["message"]["content"]
-
-def request_answer():
-    return ""
-
-
-def conversation(pages):
-    system_message = collect_system_message()
     for page in pages:
-        asking_openai(str(page), secrets)
-    response = request_answer()
-    return response
+        prompt.append({
+            "role" : "user",
+            "content" : str(page)
+        })
+    prompt.append({
+        "role" : "user",
+        "content" : "ALL GOOD, PLEASE ANSWER THE QUESTION"
+    })
+    response = await client.chat.completions.create(
+        model=deployment_name,
+        messages=prompt,
+        temperature=0.5
+    )
+    out = response.choices[0].message.content
+    out = "\n".join(out.split("\n")[1:-1])
+    return out
         
 
 
-secrets = secret_extraction()
