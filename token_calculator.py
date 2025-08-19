1ca96502b4e8d2ab884dee6a8c28264a1ae80b7c
TKartist <hunr2001@gmail.com>
2025-08-19 14:44:29 +0200

    no conflict

--- token_calculator.py
+++ token_calculator.py
@@ -10,18 +10,22 @@ def calculate_token_count(text_stream):
 
 
 
+x = "123, [421, 11, 12, 41], 21, hello my friend go"
+k = "123, 321"
+l = ""
+z = calculate_token_count(x)
+print(z)
+# l = os.listdir("output")
+# for i in l:
+#     with open(f"output/{i}", "r") as f:
+#         text = f.read()
+#         f.close()
 
-l = os.listdir("output")
-for i in l:
-    with open(f"output/{i}", "r") as f:
-        text = f.read()
-        f.close()
-
-    lists = ast.literal_eval(text)
-    count = 0
-    for list in lists:
-        count += calculate_token_count(str(list))
-    if count > 100000:
-        print(i)
-        print(count)
+#     lists = ast.literal_eval(text)
+#     count = 0
+#     for list in lists:
+#         count += calculate_token_count(str(list))
+#     if count > 100000:
+#         print(i)
+#         print(count)
 
