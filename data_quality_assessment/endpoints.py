e86e697a835ed01d6610d57781baf452c5bb0069
TKartist <hunr2001@gmail.com>
2026-02-26 10:49:11 +0100

    adding endpoints and go_api.yml file

---
+++ data_quality_assessment/endpoints.py
@@ -0,0 +1,12 @@
+ENDPOINT = {
+    "appeal" : {
+        "file_location" : "appeal.csv",
+        "endpoint" : "api/v2/appeal/"
+    },
+    "local-units" : {
+        "file_location" : "local_units.csv",
+        "endpoint" : "api/v2/local-units/"
+    }
+}
+
+ROOT = "https://goadmin.ifrc.org/"
